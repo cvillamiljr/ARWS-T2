33027c0569a209fee47a73f909ebb06f18e1f8ef
Cesar Villamil <cesar.villamil@mail.escuelaing.edu.co>
2020-10-22 12:13:53 -0500

    EsqueletoParcial

---
+++ src/main/java/edu/eci/parcialT2/ParcialT2Application.java
@@ -0,0 +1,13 @@
+package edu.eci.parcialT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialT2Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/parcialT2/ParcialT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.parcialT2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
